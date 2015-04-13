a4ead08c18c1399b4d141e0ca9bb1c6533041af1
vinicioflores <vfloreshdz@gmail.com>
2015-04-13 15:59:28 -0600

    UPDATED
    Last  version of the ¨One year on Mars¨ prototype

---
+++ Assets/Scripts/salirCueva.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class salirCueva : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerStay(Collider myCollision){
+		if (myCollision.gameObject.tag == "jugador") {
+			Application.LoadLevel(GameObject.FindWithTag("MainCamera").GetComponent<recursosCamara>().getNivel());
+		}
+	}
+}
---
+++ Assets/Scripts/tormenta.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tormenta : MonoBehaviour {
+	bool segundo=true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	// Update is called once per frame
+	void Update () {
+		if(segundo){
+			if ((int)(Time.time%2)==0) {  //%3 para cada 3 segundos (cambiar a los segundos que queremos que dure)
+				segundo=false;  //Si el tiempo es 0, como el update se hace muchas veces por segundo, cambiamos la variable segundo y asi no entrara al ciclo
+				GameObject.FindWithTag("MainCamera").GetComponent<recursosCamara>().aumentaVida(-10);
+			}
+		}
+		if ((int)(Time.time % 2) == 1) { //Cuando el tiempo este en 1, cambiara segundo y podra entrar a la funcion anterior, pero se actualizara hasta que este en 0
+			segundo=true;
+		}
+	}
+}
